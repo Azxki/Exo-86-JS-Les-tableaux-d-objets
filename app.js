2c521e84dfacfa04b9c68833182c77eaaf212782
azxki <azxki01@gmail.com>
2022-12-05 13:51:29 +0100

    EXO

--- app.js
+++ app.js
@@ -31,8 +31,22 @@ let voitures = {
     garage: 'concession',
     lieu: 'Fourmies',
     // Dans un tableau, ajouter les différentes voitures que vous possédez.
-    collection: [], // Vos voitures dans cette propriété !
+    collection: [peugeot, citroen, ford], // Vos voitures dans cette propriété !
 }
+
+for (let i = 0; i < voitures.collection.length; i++) {
+    console.log(voitures.collection[i]);
+    console.log(voitures.collection[i].ouvrirPorte());
+}
+
+let span = document.createElement('span');
+
+let para = document.querySelector("p").appendChild(span);
+
+para.innerHTML = "Citroen " + citroen.color + "<br>"
+para.innerHTML += "Ford " + ford.color + "<br>"
+para.innerHTML += "Peugeot " + peugeot.color
+
 
 // FIXME Parcourrez le tableau dans lequel vous avez placé vos objets.
 // TODO ==> invoquez la méthode ouvrirPorte pour chaque voiture de la collection.
